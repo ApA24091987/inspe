98e99db0fdcce0d875a9efd3232db0d42c37508d
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-10-22 21:22:34 +0300

    Update inspe.py

--- inspe.py
+++ inspe.py
@@ -1,38 +1,31 @@
 import inspect
 
+
 def introspection_info(obj):
-    # Тип объекта
     obj_type = type(obj).__name__
 
-    # Атрибуты и методы объекта
     attributes_and_methods = dir(obj)
     methods = [attr for attr in attributes_and_methods if callable(getattr(obj, attr))]
+    attributes = [attr for attr in attributes_and_methods if not callable(getattr(obj, attr))]
 
-    # Модуль, к которому объект принадлежит
     module = inspect.getmodule(obj)
     module_name = module.__name__ if module else '__main__'
 
-    # Другие интересные свойства объекта
     other_properties = {}
     if hasattr(obj, '__doc__'):
         other_properties['doc'] = obj.__doc__
     if hasattr(obj, '__class__'):
         other_properties['class'] = obj.__class__.__name__
-    if hasattr(obj, '__dict__'): # Для объектов с атрибутами
+    if hasattr(obj, '__dict__'):
         other_properties['instance_variables'] = obj.__dict__
-    if hasattr(obj, '__name__'): # Для функций и классов
+    if hasattr(obj, '__name__'):
         other_properties['name'] = obj.__name__
-    if hasattr(obj, '__qualname__'): # Для функций и классов
+    if hasattr(obj, '__qualname__'):
         other_properties['qualname'] = obj.__qualname__
 
-    # Классовая иерархия (для классов)
-    if inspect.isclass(obj):
-        other_properties['class_hierarchy'] = inspect.getmro(obj)
-
-    # Собираем все данные в словарь
     info = {
         'type': obj_type,
-        'attributes': attributes_and_methods,
+        'attributes': attributes,
         'methods': methods,
         'module': module_name,
         'other_properties': other_properties
@@ -40,7 +33,7 @@ def introspection_info(obj):
 
     return info
 
-# Пример использования
+
 number_info = introspection_info(42)
 print(number_info)
 
@@ -49,15 +42,3 @@ print(class_info)
 
 list_info = introspection_info([1, 2, 3])
 print(list_info)
-
-# Cвой класс и объект для понимания.
-class CustomClass:
-    def __init__(self, name):
-        self.name = name
-
-    def greet(self):
-        return f"Hello, {self.name}!"
-
-custom_obj = CustomClass("John")
-custom_obj_info = introspection_info(custom_obj)
-print(custom_obj_info)
